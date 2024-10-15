9b61edac25b677365ebf712675334d2e11dd7cb1
ReddIndiann <odaniel.ac@st.vvu.edu.gh>
2024-10-15 15:10:48 +0000

    userentry

--- src/models/packages.ts
+++ src/models/packages.ts
@@ -6,10 +6,12 @@ class Packages extends Model {
   public name!: string;
   public type!: string;
   public price!: number;
+  public bonusrate!: number;
   public rate!: number;
-  public smscount!: string;
-  public expiry!: string;
-  public duration!: number;
+  public smscount!: number;
+  public expiry!: boolean;
+  public duration!: number | null;  // Updated to allow null
+  public userEntry!: boolean; 
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
 }
@@ -30,25 +32,31 @@ Packages.init(
       allowNull: false,
     },
     price: {
-        type: DataTypes.FLOAT, // Changed to FLOAT for decimal support
-        allowNull: false,
+      type: DataTypes.FLOAT,
+      allowNull: true,  // Allow the field to be null
     },
+    bonusrate: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },0000000000000000000000000
     rate: {
-        type: DataTypes.FLOAT, // Changed to FLOAT for decimal support
-        allowNull: false,
+      type: DataTypes.FLOAT,
+      allowNull: false,
     },
     smscount: {
-        type: DataTypes.INTEGER, // Confirm if this should be INTEGER
-        allowNull: false,
+      type: DataTypes.INTEGER,
+      allowNull: true,
     },
-    
-    expiry:
-     {
-      type: DataTypes.STRING,
+    expiry: {
+      type: DataTypes.BOOLEAN,
       allowNull: false,
     },
     duration: {
       type: DataTypes.INTEGER,
+      allowNull: true,  // Allow null for duration
+    },
+    userEntry: {
+      type: DataTypes.BOOLEAN,
       allowNull: false,
     },
   },
